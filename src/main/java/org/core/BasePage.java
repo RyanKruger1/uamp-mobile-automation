9f0b0f65b677b7752c1c485f6021b257293e729b
ryank <ryank@globalkinetic.com>
2023-07-30 18:02:23 +0200

    Fix allure and lombok issues

--- src/main/java/org/core/BasePage.java
+++ src/main/java/org/core/BasePage.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.List;
 
-
 public class BasePage {
 
     @AndroidFindBy(id = "com.example.android.uamp:id/title")
